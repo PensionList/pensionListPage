82029eca465ed09befdc649950b3c002a9324449
DESKTOP-3JM81CO\PC <doujinsearcher@gmail.com>
2019-04-15 17:29:30 +0900

    backup pension list page

---
+++ js/creative.js
@@ -0,0 +1,81 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
+
+$( document ).ready(function() {
+	
+	var point = getParameter('pension');
+	if (typeof point =="undefined") {
+		return false;
+	};
+	
+	setTimeout("moveTo(" + point + ")", 2000);
+});
+
+function moveTo(point) {
+	var $anchor = $(point);
+	$('html, body').stop().animate({
+		scrollTop: ($($anchor).offset().top - 50)
+	}, 1250, 'easeInOutExpo');
+	event.preventDefault();
+}
+
+
+
+var getParameter = function (param) {
+    var returnValue;
+    var url = location.href;
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+    for (var i = 0; i < parameters.length; i++) {
+        var varName = parameters[i].split('=')[0];
+        if (varName.toUpperCase() == param.toUpperCase()) {
+            returnValue = parameters[i].split('=')[1];
+            return decodeURIComponent(returnValue);
+        }
+    }
+};
